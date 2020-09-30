66603d13853d693a189499310cedad6d2b74bed5
gornitskiy <flex.zt@gmail.com>
2020-10-01 01:59:58 +0300

    AddRequestBuilder

--- src/CodeBase/Callbacks/SignatureCallback.php
+++ src/CodeBase/Callbacks/SignatureCallback.php
@@ -4,24 +4,29 @@
 namespace ItlabStudio\ApiClient\CodeBase\Callbacks;
 
 
+use ItlabStudio\ApiClient\CodeBase\ApiResources\ControlPanel\ApiResource;
 use ItlabStudio\ApiClient\CodeBase\Interfaces\ApiResourceInterface;
 use ItlabStudio\ApiClient\CodeBase\Exceptions\BadResponceException;
 
 /**
  * Class SignatureCallback
+ *
  * @package ItlabStudio\ApiClient\CodeBase\Callbacks
  */
 class SignatureCallback extends Observer
 {
     /**
      * @param ApiResourceInterface $observable
-     * @param $response
+     * @param                      $response
+     *
      * @return bool
      * @throws BadResponceException
      */
     function goal(ApiResourceInterface $observable, $response)
     {
-        if (true) {
+        if ($observable instanceof ApiResource) {
+            return $response->toArray();
+        } elseif ($observable instanceof ApiResourceInterface) {
             return $response;
         } else {
             throw new BadResponceException('Signature does not match to the response body');

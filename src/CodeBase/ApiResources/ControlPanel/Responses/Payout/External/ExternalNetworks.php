c276855ac6a9107d4bad3201fe85b0fc73047242
flexzt <flex.zt@gmail.com>
2021-01-12 14:11:14 +0200

    fix city structure (#36)

---
+++ src/CodeBase/ApiResources/ControlPanel/Responses/Payout/External/ExternalNetworks.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace ItlabStudio\ApiClient\CodeBase\ApiResources\ControlPanel\Responses\Payout\External;
+
+
+use ItlabStudio\ApiClient\CodeBase\ApiResources\ControlPanel\Responses\Payout\external;
+
+/**
+ * Class ExternalNetworks
+ *
+ * @package ItlabStudio\ApiClient\CodeBase\ApiResources\ControlPanel\Responses\Payout\External
+ */
+class ExternalNetworks
+{
+
+    /**
+     * @var MiccNetwork
+     */
+    protected MiccNetwork $network;
+
+    /**
+     * @var MiccCities[] $cities
+     */
+    protected array $cities;
+
+    /**
+     * @return MiccNetwork
+     */
+    public function getNetwork(): MiccNetwork
+    {
+        return $this->network;
+    }
+
+    /**
+     * @param MiccNetwork $network
+     *
+     * @return external
+     */
+    public function setNetwork(MiccNetwork $network): ExternalNetworks
+    {
+        $this->network = $network;
+
+        return $this;
+    }
+
+    /**
+     * @return MiccCities[]
+     */
+    public function getCities(): array
+    {
+        return $this->cities;
+    }
+
+    /**
+     * @param MiccCities[] $cities
+     *
+     * @return external
+     */
+    public function setCities(array $cities): ExternalNetworks
+    {
+        $this->cities = $cities;
+
+        return $this;
+    }
+}
